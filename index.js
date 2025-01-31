5e9e7471e400aefdc7a963ab7bdd41607b5dd33e
Frankiegit config --global user.name Frankiegit config --global user.name Frankie <Frankiebrown996@gmail.com>
2025-01-31 06:17:03 +0000

    bug fixstarts here

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import   menuArray   from './data.js';
 let basket = [];
 console.log(menuArray)
+
+
+document.addEventListener('click', (event)=>{
+    if (event.target.classList.contains('btn')){
+        console.log(event.target.id)
+        handleAddToCart(event.target.id)
+       
+    }
+})
+
+function handleAddToCart(event){
+    const menuItem = menuArray.find((menuItem)=>menuItem.id===parseInt(event.target.id));
+    basket.push(menuItem);
+    console.log(basket)
+   
+}
+
 function getMenuHtml(){
     let menuHtml=" ";
     menuArray.forEach((menuItem)=>{            
@@ -9,25 +26,13 @@ function getMenuHtml(){
             <h3 class="name">${menuItem.name}</h3>
             <p class="ingredients">${menuItem.ingredients.join(", ")}</p>
             <p class="price">${menuItem.price}</p>
-            <button class="btn" data-add="${menuItem.id}"=>+</button>
+            <button class="btn" data-add="${menuItem.id}">+</button>
             </div>`
 
      })
     return menuHtml;
 }
 
-document.addEventListener('click', (event)=>{
-    if (event.target.classList.contains('btn')){
-        console.log(event.target.id)
-    }
-})
-
-function handleAddToCart(event){
-    const menuItem = menuArray.find((menuItem)=>menuItem.id===parseInt(event.target.id));
-    basket.push(menuItem);
-    console.log(basket)
-
-}
 
 function render(){
     document.getElementById('menu').innerHTML=getMenuHtml();
